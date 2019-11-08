cf9be89a347d58e0c6e63b91b1d33653e9f8a9c3
GeorQ <georqv2@icloud.com>
2019-11-08 14:32:11 +0000

    Coursework

--- Coursework/spellchecker222.py
+++ Coursework/spellchecker222.py
@@ -1,3 +1,74 @@
-file_name = input("Enter the file name")
-dictionary = open(file_name, "rt").read()
-print(dictionary)
+# file_name = input("Enter the file name")
+# dictionary = open(file_name, "rt").read()
+# print(dictionary)
+
+
+
+def main_scene():
+	print("\n" + ("  █" + "▀"+"▀"*43+"█\n") + ("  █" + "   S P E L L   C H E C K E R "+" "*15+"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + "     1. Check a file "+" "*23+"█\n") + ("  █" + "     1. Check a sentense "+" "*19+"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + "     0. Quit "+" "*31+"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + "▄"+"▄"*43+"█")) #▂
+	choice = input(("  █\n") + ("  █" + "▄▄" * 9 + "▄█"+" Enter choice: "))
+	return choice
+
+def file_scene():
+	print("\n" + ("  █" + "▀"+"▀"*43+"█\n") + ("  █" + "    L O A D   F I L E "+" "*22+"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + "      Enter the file name "+" "*18+"█\n") + ("  █" + "      then press [enter] "+" "*19+"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + "▄"+"▄"*43+"█")) #▂
+	file_name = input(("  █\n") + ("  █" + "▄▄" * 6 + "▄█"+" File name: "))
+	return file_name
+
+def first_word_scene(word, different_word):
+	print("\n" + ("  █" + "▀"+"▀"*43+"█\n") + ("  █" + "    W O R D   N O T   F O U N D "+" "*12+"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + "      "+ word +" "*(44 - int(len(word)) - 6)+"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + "      Did you mean " + " "*25+"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + "      " + different_word + " " * (44 - int(len(different_word)) - 6) +"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + "▄"+"▄"*43+"█")) #▂
+	choice = input(("  █\n") + ("  █" + "▄▄" * 6 + "▄█"+" Enter [y] or [n]: "))
+	return choice
+
+def second_word_scene():
+	print("\n" + ("  █" + "▀"+"▀"*43+"█\n") + ("  █" + "    W O R D   N O T   F O U N D "+" "*12+"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + "      "+ word +" "*(44 - int(len(word)) - 6)+"█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + "      1. Ignore the word. " + " " * 18 + "█\n") + ("  █" + "      1. Mark the word as incorrect. " + " " * 7 + "█\n") + ("  █" + "      1. Add the word to dictionary. " + " " * 7 + "█\n") + ("  █" + " "+" "*43+"█\n") + ("  █" + "▄"+"▄"*43+"█")) #▂
+	choice = input(("  █\n") + ("  █" + "▄▄" * 6 + "▄█"+" Enter choice: "))
+	return choice
+
+
+word = "lofesfsssssssssssssss"
+
+different_word = "flelelfe"
+
+
+
+input("Enter")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(("  "+"▂" *40))																																																																						  
+																																																																											# " "
+# +"▃"*52 + "▐ ▌,▛) ("  ▌" + "▃"+"▃"*24+"  ▐")
+# input("\n\n\nEnter")▌
+# ▐▐
+# ▌▌▐▐
+# mode_choice = input("\t  1. Check a file\n\t  2. Check a sentense\n\n\t  0. Quit\n\n Enter choice: ")
--- Coursework/spellchecker4.py
+++ Coursework/spellchecker4.py
@@ -1,15 +1,18 @@
 import time, datetime
 
-while True:
+while True: #main loop, which allow us to work with program as long as we want
 	total = 0
 	correct = 0
 	wrong = 0
-	alpha_char = "qwertyuioplkmnjhbgvfcxdzsa " # var of characters and space to keep words separatly
+	wrong_ignor = 0
+	wrong_mark = 0
+	wrong_dict = 0
+	alpha_char = "qwertyuioplkmnjhbgvfcxdzsa " # var of alpha characters and space to keep words separatly
 	print("\tS P E L L   C H E C K E R\n")
 	mode_choice = input("\t  1. Check a file\n\t  2. Check a sentense\n\n\t  0. Quit\n\n Enter choice: ")
-	if mode_choice == "0":
+	if mode_choice == "0": #if they want to quit from the application 
 		break
-	elif mode_choice == "1":
+	elif mode_choice == "1": # mode for checking the file
 		file_name = input(" Enter the name of the file to spellcheck: ")
 		start = time.time()
 		file_to_check = open(file_name, "rt")
@@ -19,12 +22,12 @@ while True:
 		file_content = file_content.lower() # make all character in sentense lower case, because all word in dictionry have lower case
 
 
-		for letter in file_content: # This loop cheack for alpha chars in words and if it now alpha char it delete it
+		for letter in file_content: # This loop cheack for alpha chars in words and if they are not alpha char it delete it
 			if letter not in alpha_char:
 				file_content = file_content.replace(letter, "")
 
 
-		file_content = file_content.split()
+		file_content = file_content.split() #now this became a list, we should use list here because?
 		dictionary = open("01_EnglishWords.txt", "rt")
 		test = dictionary.read() #put all words from file 0_1EnglishWords.txt inside the var "test"
 		dictionary.close()
@@ -41,8 +44,7 @@ while True:
 				wrong_words.append(word)
 		end = time.time()
 
-		print("\n Number of words: ",total, "\n Number of correctly spelt words: ", correct, "\n Number of incorrectly spelt words: ", wrong, "\n")
-		print(" Time elapsed ",(int((end - start)* 10**6)), " microseconds")
+
 
 		now_time = datetime.datetime.now()
 
@@ -53,14 +55,41 @@ while True:
 			if word not in wrong_words:
 				report_list += word + " "
 			else:
-				word = "?" + word + "? "
-				report_list += word
+				decision = input("\n" + word + " not found\n\n\n 1.  Ignore the word.\n\n\n 2.  Mark the word as incorrect.\n\n\n 3.  Add word to dictionary.\n\n\n Enter choice: ")
+			
+				while (decision != "1" and decision != "2" and decision != "3"):
+					decision = input("Please choose one of the three options!\n Enter option: ") 
+
+				if decision == "1":
+					word = "!" + word + "! "
+					report_list += word
+					wrong_ignor += 1
+				elif decision == "2":
+					word = "?" + word + "? "
+					report_list += word
+					wrong_mark += 1
+				elif decision == "3":
+					dictionary = open("01_EnglishWords.txt", "a")
+					dictionary = dictionary.write("\n" + word)
+					dictionary.close()#whant to change
+					word = "*" + word + "* "
+					report_list += word
+					wrong_dict += 1
+		print("\n Number of words: ",total, "\n Number of correctly spelt words: ", correct, "\n Number of incorrectly spelt words: ", wrong, "\n\t Number ignored: " + str(wrong_ignor)+ "\n\t Number added to dictionary: " + str(wrong_dict) + "\n\t Numer marked: " + str(wrong_mark) + "\n")
+		print(" Time elapsed ",(int((end - start)* 10**6)), " microseconds")
 
 		report = open("report.txt", "w")
 		report.write(report_list)
 		report.close()
 
-	elif mode_choice == "2":
+
+
+
+
+
+
+
+	elif mode_choice == "2": #mode for checking the sentense
 		sentense = input("Please enter the sentense, which you want to check: ")
 		# sentense = "lol1 121l  flf1'fe1     lfefk.     21;f lfe lfefe wsqsa cxczxas qwe asx"
 		sentense = sentense.lower() # make all character in sentense lower case, because all word in dictionry have lower case
@@ -84,8 +113,8 @@ while True:
 
 		total = len(sentense)
 
-		for x in sentense: # Loop will
-			if x in test: #check for every word in sentense to be in dictionaty or not
+		for x in sentense: # Loop will check for every word in sentense to be in dictionaty or not
+			if x in test: 
 				correct += 1
 				print(x + " spelt correctly")
 			else:
@@ -102,7 +131,13 @@ while True:
 		if answer == "q" or answer == "Q":
 			break
 		else:
-			print("\n\n") #some space to begin with?
+			print("\n\n") #some space to begin with
 			continue
+
+	else: 
+		print("\n\n") #some space to begin with
+		continue
+
+
 
 input("Enter")
